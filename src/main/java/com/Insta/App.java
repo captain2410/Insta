d3209029c4fb1f57b4efb0cc57dacc5e409b788f
captain2410 <241069ns@gmail.com>
2023-07-08 19:15:44 +0530

    Insta

---
+++ src/main/java/com/Insta/App.java
@@ -0,0 +1,13 @@
+package com.Insta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
